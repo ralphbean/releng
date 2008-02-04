e6387fbfeef2f85a073029abffc3f4c67c89dbac
Jesse Keating <jkeating@redhat.com>
2008-02-04 17:33:54 -0500

    Create a script to check which packages need to be rebuilt for the new gcc4.3.

---
+++ scripts/need-rebuild-gcc43.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+# template for finding builds that meet some time/buildroot component critera.
+# Edit to suit.
+
+import koji
+
+kojisession = koji.ClientSession('http://koji.fedoraproject.org/kojihub')
+tocheck = []
+needbuild = []
+reallyneedbuild = []
+
+f9builds = kojisession.listTagged('dist-f9', inherit=True, latest=True)
+
+for build in f9builds:
+    if build['creation_time'] < '2008-01-30 15:22:10.000000':
+        tocheck.append(build)
+
+checknum = len(tocheck)
+
+for build in tocheck:
+    print "Checking %s (%s of %s)" % (build['nvr'], tocheck.index(build)+1, checknum)
+    for task in kojisession.getTaskChildren(build['task_id']):
+        if build in needbuild:
+            continue
+        if task['method'] == 'buildArch':
+            for rootid in kojisession.listBuildroots(taskID=task['id']):
+                for pkg in kojisession.listRPMs(componentBuildrootID=rootid['id']):
+                    if pkg['name'] == 'gcc':
+                        if pkg['version'] < '4.3.0':
+                            if not build in needbuild:
+                                print "adding", build['name']
+                                needbuild.append(build)
+                                continue
+            continue
+
+rebuildnames = []
+for build in needbuild:
+    if not build in reallyneedbuild:
+        reallyneedbuild.append(build)
+        rebuildnames.append(build['name'])
+
+rebuildnames.sort()
+for build in rebuildnames:
+    print build
