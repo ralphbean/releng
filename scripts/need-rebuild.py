39efd28590f03c87aa5da62de15e5965eb731436
Jesse Keating <jkeating@redhat.com>
2009-02-25 14:39:03 -0800

    Revamp need-rebuild so that it can use multiCall more.

--- scripts/need-rebuild.py
+++ scripts/need-rebuild.py
@@ -16,6 +16,8 @@ import os
 buildtaglist = ['dist-f11', 'dist-f11-rebuild'] # tag(s) to check
 epoch = '2009-02-23 18:31:07.000000' # rebuild anything not built after this date
 tobuild = {} # dict of owners to lists of packages needing to be built
+built = [] # raw list of built packages
+unbuilt = [] # raw list of unbuilt packages
 
 # Create a koji session
 kojisession = koji.ClientSession('https://koji.fedoraproject.org/kojihub')
@@ -36,33 +38,40 @@ kojisession.multicall = True
 for pkg in pkgs:
     name = pkg['package_name']
     id = pkg['package_id']
-
-    # Query to see if a build has already been attempted
-    # this version requires newer koji:
-    #  if  kojisession.listBuilds(id, createdAfter=epoch):
-    # This version won't catch builds in flight
     kojisession.listBuilds(id, completeAfter=epoch)
 
 # Get all the results
 results = kojisession.multiCall()
 
-# Loop through the results and build up a list of things to build
-for pkg, result in zip(pkgs, results):
-    newbuild = 0
-    if result[0]:
-        for build in result[0]:
-            buildtarget = kojisession.getTaskInfo(build['task_id'],
-                                       request=True)['request'][1]
-            if buildtarget in buildtaglist:
-                # We've already got an attempt made, skip.
-                newbuild = True
-                break
+# For each build, get it's request info
+kojisession.multicall = True
+for result in results:
+    for oldbuild in result[0]:
+        kojisession.getTaskInfo(oldbuild['task_id'],
+                                request=True)
 
-    if not newbuild:
+# For each request, compare to our target and populate the newbuild list.
+requests = kojisession.multiCall()
+for request in requests:
+    tasktarget = request[0]['request'][1]
+    taskpkg = request[0]['request'][0].rsplit('/')[-2]
+    if tasktarget in buildtaglist:
+        if not taskpkg in built:
+            built.append(taskpkg)
+
+# Loop through the results and build up a list of things to build
+for pkg in pkgs:
+    pkgname = pkg['package_name']
+    if not pkgname in built:
+        unbuilt.append(pkgname)
         if tobuild.has_key(pkg['owner_name']):
-            tobuild[pkg['owner_name']].append(pkg['package_name'])
+            tobuild[pkg['owner_name']].append(pkgname)
         else:
-            tobuild[pkg['owner_name']] = [pkg['package_name']]
+            tobuild[pkg['owner_name']] = [pkgname]
+
+print "%s unbuilt packages:" % len(unbuilt)
+
+print sorted(unbuilt)
 
 # Print the results
 for owner in sorted(tobuild.keys()):
