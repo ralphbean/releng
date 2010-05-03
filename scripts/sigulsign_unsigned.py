5e37b40e663d6cd0982df13f21d7c6df0e0cd984
Dennis Gilmore <dennis@ausil.us>
2010-05-03 14:41:40 -0500

    add epel-6 to sigulsign_unsigned.py

--- scripts/sigulsign_unsigned.py
+++ scripts/sigulsign_unsigned.py
@@ -47,7 +47,8 @@ KEYS = {'fedora-12': {'id': '57bbccba', 'v3': True},
         'fedora-13': {'id': 'e8e40fde', 'v3': True},
         'fedora-11': {'id': 'd22e77f2', 'v3': True},
         'fedora-10': {'id': '4ebfc273', 'v3': False},
-        'fedora-10-testing': {'id': '0b86274e', 'v3': False}}
+        'fedora-10-testing': {'id': '0b86274e', 'v3': False},
+        'epel-6': {'id': '0608b895', 'v3': True}}
 
 # Throw out some functions
 def writeRPMs():
