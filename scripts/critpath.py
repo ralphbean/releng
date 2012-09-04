c86e1f1b805b213c230de6935a1f8053d021b687
Bill Nottingham <notting@redhat.com>
2012-09-04 12:03:32 -0400

    Update critpath versions

--- scripts/critpath.py
+++ scripts/critpath.py
@@ -42,12 +42,12 @@ updatepath = {
     'rawhide': ''
 }
 
-for r in ['12', '13', '14', '15', '16']: # 13, 14, ...
+for r in ['12', '13', '14', '15', '16', '17']: # 13, 14, ...
     releasepath[r] = 'releases/%s/Everything/$basearch/os/' % r
     updatepath[r] = 'updates/%s/$basearch/' % r
 
 # Branched Fedora goes here
-branched = '17'
+branched = '18'
 releasepath['branched'] = 'development/%s/$basearch/os' % branched
 updatepath['branched'] = ''
 
