33e985d91f5f62ed2f0da9ad1b21e7624902ca87
Dennis Gilmore <dennis@ausil.us>
2014-05-29 18:48:15 -0500

    setup mass-tag..py for f21-python merging

--- scripts/mass-tag.py
+++ scripts/mass-tag.py
@@ -15,8 +15,8 @@ import operator
 
 # Set some variables
 # Some of these could arguably be passed in as args.
-target = 'f20' # tag to tag into
-holdingtag = 'f20-rebuild' # tag holding the rebuilds
+target = 'f21' # tag to tag into
+holdingtag = 'f21-python' # tag holding the rebuilds
 newbuilds = {} # dict of packages that have a newer build attempt
 tasks = {} # dict of new build task info
 
