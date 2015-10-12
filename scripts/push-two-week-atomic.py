7640b920d6371bf3ff7409f7892e375d9b8844c3
Adam Miller <admiller@redhat.com>
2015-10-12 16:05:18 -0500

    recursively removedirs for pruning

--- scripts/push-two-week-atomic.py
+++ scripts/push-two-week-atomic.py
@@ -391,7 +391,7 @@ def prune_old_testing_composes(
 
     for testing_dir in prune_candidate_dirs[prune_limit:]:
         try:
-            os.rmdir(
+            os.removedirs(
                 os.path.join(prune_base_dir, testing_dir)
             )
         except OSError, e:
