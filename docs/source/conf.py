396e1841ac9ec3cca70785d6123e51fe2027b82e
Till Maas <opensource@till.name>
2015-11-08 10:16:09 +0100

    Adjust copyright notice to wiki
    Signed-off-by: Till Maas <opensource@till.name>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,7 +55,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Fedora Release Engineering'
-copyright = u'2015, Red Hat'
+copyright = u'2015, Red Hat, Inc. and others'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
